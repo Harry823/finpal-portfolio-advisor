a875ae23bd15ccf47ed0349af5d4c2e428fcfde9
Harry823 <hdtjahja@gmail.com>
2025-08-22 17:12:41 -0700

    formatted mvp ui

--- frontend/src/app/components/SpeechButton.tsx
+++ frontend/src/app/components/SpeechButton.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
 const SpeechButton = () => {
@@ -6,25 +7,55 @@ const SpeechButton = () => {
         transcript,
         listening,
         resetTranscript,
-        browserSupportsSpeechRecognition
     } = useSpeechRecognition();
+    const [loadingResponse, setLoadingResponse] = useState<boolean>(false);
+    // TODO: plug error message in state and display
+    const [errorMessage, setErrorMessage] = useState<string>();
+    const [responseText, setResponseText] = useState<string>('');
+
+
+    const getAiResponse = async () => {
+        setLoadingResponse(true);
+        console.log('running ai response')
+        try {
+            const inputJSON = {
+                text: transcript
+            };
+            console.log(inputJSON);
+            // TODO: use the given API to interact with AI
+            setTimeout(() =>  setResponseText('test response'), 5000)
+        } catch(error) {
+            console.log('error:', error);
+        } finally {
+            setLoadingResponse(false);
+        }
+
+    }
+
+    const onClickStop = async () => {
+        SpeechRecognition.stopListening()
+        await getAiResponse();
+    }
     
     return (
         <div
-            className="flex gap-3 flex-col items-center justify-center"
+            className="mt-10 gap-3 flex flex-col items-center justify-center"
         >
+            <p>Talk to Finpal and get feedback on your portfolio</p>
             <p>Microphone: {listening ? 'on' : 'off'}</p>
             <div
                 className='flex gap-2'
             >
                 <button
-                    onClick={SpeechRecognition.startListening}
-                    className='px-3 p-1 bg-white rounded-3xl text-black'
+                    onClick={() => SpeechRecognition.startListening()}
+                    className={
+                        `px-3 p-1 bg-white rounded-3xl text-black`
+                    }
                 >
                     Start
                 </button>
                 <button
-                    onClick={SpeechRecognition.stopListening}
+                    onClick={onClickStop}
                     className='px-3 p-1 bg-white rounded-3xl text-black'
                 >
                     Stop
@@ -36,7 +67,10 @@ const SpeechButton = () => {
                     Reset
                 </button>
             </div>
+            <p className='mt-5'>Voice Input</p>
             <p>{transcript}</p>
+            <p className='mt-5'>Response</p>
+            <p>{loadingResponse ? 'thinking' : responseText}</p>
         </div>
     );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
 
   return (
     <div className="py-10 flex flex-col items-center justify-center">
-      <h1 className="my-2">Finpal</h1>
+      <h1 className="text-5xl">Finpal</h1>
+      <h2 className="text-xl">Your AI Financial Portfolio Advisor</h2>
       <SpeechButton />
     </div>
   );
